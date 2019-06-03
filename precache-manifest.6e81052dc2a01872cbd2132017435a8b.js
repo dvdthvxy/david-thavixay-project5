0080ab0bf636daefbf124e16f35578a56dfd0808
David Thavixay <david.thavixay@gmail.com>
2019-06-03 09:53:35 -0400

    Updates

---
+++ precache-manifest.6e81052dc2a01872cbd2132017435a8b.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac93b725866141aa15c5009f2dfa66be",
+    "url": "/david-thavixay-project5/index.html"
+  },
+  {
+    "revision": "84cf4bcbd646ca2ef632",
+    "url": "/david-thavixay-project5/static/css/main.6ade4023.chunk.css"
+  },
+  {
+    "revision": "16f7e44cd2ef138bf1f9",
+    "url": "/david-thavixay-project5/static/js/2.896995ef.chunk.js"
+  },
+  {
+    "revision": "84cf4bcbd646ca2ef632",
+    "url": "/david-thavixay-project5/static/js/main.3bc755f6.chunk.js"
+  },
+  {
+    "revision": "e496dc3190c460f1612b",
+    "url": "/david-thavixay-project5/static/js/runtime~main.6db92c80.js"
+  },
+  {
+    "revision": "44d9a8c39a5a6eec5cefae4c39e04913",
+    "url": "/david-thavixay-project5/static/media/ATL.44d9a8c3.png"
+  },
+  {
+    "revision": "dfe8d42a5600a4168edb41c350f706f4",
+    "url": "/david-thavixay-project5/static/media/BKN.dfe8d42a.png"
+  },
+  {
+    "revision": "ce31fa8720b1000ba93c4bf8ba27e1a7",
+    "url": "/david-thavixay-project5/static/media/BOS.ce31fa87.png"
+  },
+  {
+    "revision": "12f7c3f1b4c4108d9b8ab3eea43cbbe5",
+    "url": "/david-thavixay-project5/static/media/CHA.12f7c3f1.png"
+  },
+  {
+    "revision": "2ce0389be519b538a8b69de888b56c10",
+    "url": "/david-thavixay-project5/static/media/CHI.2ce0389b.png"
+  },
+  {
+    "revision": "11b01aaca1768a3560f6df3abb48f02c",
+    "url": "/david-thavixay-project5/static/media/CLE.11b01aac.png"
+  },
+  {
+    "revision": "159f2b2fd94b09881617dae454583a1d",
+    "url": "/david-thavixay-project5/static/media/DAL.159f2b2f.png"
+  },
+  {
+    "revision": "64648b1c7af0f60e6b411058c120657b",
+    "url": "/david-thavixay-project5/static/media/DEN.64648b1c.png"
+  },
+  {
+    "revision": "3805cf1981b1534bad0dd56c6af4189e",
+    "url": "/david-thavixay-project5/static/media/DET.3805cf19.png"
+  },
+  {
+    "revision": "004510f96a9bc94f6a2fbfdfbaab8028",
+    "url": "/david-thavixay-project5/static/media/GSW.004510f9.png"
+  },
+  {
+    "revision": "f519f52545107cdd95e2dd60113da9d2",
+    "url": "/david-thavixay-project5/static/media/HOU.f519f525.png"
+  },
+  {
+    "revision": "95eb72c1f92305a40ef511c3672f4407",
+    "url": "/david-thavixay-project5/static/media/IND.95eb72c1.png"
+  },
+  {
+    "revision": "62db47d61544d84a26512c31d5b82a98",
+    "url": "/david-thavixay-project5/static/media/LAC.62db47d6.png"
+  },
+  {
+    "revision": "e2d7172633929782de6b68491234c8ab",
+    "url": "/david-thavixay-project5/static/media/LAL.e2d71726.png"
+  },
+  {
+    "revision": "ef5f657341bb8392068b2701666d9002",
+    "url": "/david-thavixay-project5/static/media/MEM.ef5f6573.png"
+  },
+  {
+    "revision": "fe66b876178699405d6498b2566cab94",
+    "url": "/david-thavixay-project5/static/media/MIA.fe66b876.png"
+  },
+  {
+    "revision": "22ec9c9def53121c0c0b8fcfa6d77513",
+    "url": "/david-thavixay-project5/static/media/MIL.22ec9c9d.png"
+  },
+  {
+    "revision": "3221326275a6a2c41bc184fe0e83dd8b",
+    "url": "/david-thavixay-project5/static/media/MIN.32213262.png"
+  },
+  {
+    "revision": "a92c1840f53fe1127dc7d470e2e944ec",
+    "url": "/david-thavixay-project5/static/media/NOP.a92c1840.png"
+  },
+  {
+    "revision": "7bea0d633eaca8c5f0a1a2001a5d677a",
+    "url": "/david-thavixay-project5/static/media/NYK.7bea0d63.png"
+  },
+  {
+    "revision": "49d52de5edee381ee655f56de8f1caa4",
+    "url": "/david-thavixay-project5/static/media/OKC.49d52de5.png"
+  },
+  {
+    "revision": "702d5cd26408ffe356ab49dbd8bd39a9",
+    "url": "/david-thavixay-project5/static/media/ORL.702d5cd2.png"
+  },
+  {
+    "revision": "29ad8e6c8e91604793bdaf92a6ae7fdb",
+    "url": "/david-thavixay-project5/static/media/PHI.29ad8e6c.png"
+  },
+  {
+    "revision": "8f68c9b6ad2509321195b0da2f275cf4",
+    "url": "/david-thavixay-project5/static/media/PHO.8f68c9b6.png"
+  },
+  {
+    "revision": "9f6b54c45eceddba1a055fae004923fb",
+    "url": "/david-thavixay-project5/static/media/POR.9f6b54c4.png"
+  },
+  {
+    "revision": "997f9054f7c7c81902c25bb935846d3f",
+    "url": "/david-thavixay-project5/static/media/SAC.997f9054.png"
+  },
+  {
+    "revision": "30e678eb001e1f8996172df73ccac095",
+    "url": "/david-thavixay-project5/static/media/SAS.30e678eb.png"
+  },
+  {
+    "revision": "2f461b127b9eef02e548c1742cc2dba2",
+    "url": "/david-thavixay-project5/static/media/TOR.2f461b12.png"
+  },
+  {
+    "revision": "47cf76c18a06fab8fb029ce0729508f4",
+    "url": "/david-thavixay-project5/static/media/UTA.47cf76c1.png"
+  },
+  {
+    "revision": "8b397652e8783c4cc98b46d74eb604a8",
+    "url": "/david-thavixay-project5/static/media/WAS.8b397652.png"
+  },
+  {
+    "revision": "890a748c1a03b7f34380069cef9a9441",
+    "url": "/david-thavixay-project5/static/media/background.890a748c.jpg"
+  },
+  {
+    "revision": "9a556e31936b83ff3bebfbca9acc01c4",
+    "url": "/david-thavixay-project5/static/media/cryingjordan.9a556e31.png"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/david-thavixay-project5/static/media/noPicture.b6681619.png"
+  }
+]);
